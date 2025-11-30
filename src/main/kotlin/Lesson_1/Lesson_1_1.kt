821e26bf46aab6d4866a3b373e757344666bc4e4
vakerimov <duka1987@gmail.com>
2025-11-30 21:58:26 +0300

    Commit Lesson 1_1

---
+++ src/main/kotlin/Lesson_1/Lesson_1_1.kt
@@ -0,0 +1,8 @@
+package org.example.Lesson_1
+
+fun main() {
+    var numberOfOrders: Int = 75
+    var thanksMessage: String = "Спасибо за заказ!"
+    println(numberOfOrders)
+    println(thanksMessage)
+}
